a61ffa233813f140138894fe8c455fd00fdb0e41
FishPhd <samuel.fish@live.com>
2016-02-21 19:46:36 -0800

    Setting up shop

---
+++ H2V/Cfg.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace H2V
+{
+  internal static class Cfg
+  {
+    public static Dictionary<string, string> IniFile = new Dictionary<string, string>();
+
+    public static void Initial(string error)
+    {
+      var iniExists = LoadConfigFile("xlive.ini", ref IniFile);
+
+      if (!iniExists)
+      {
+        SetVariable("profile name 1 =", "", ref IniFile);
+        SetVariable("profile xuid 1 =", "0000000000000000", ref IniFile);
+      }
+
+      if (!SaveConfigFile("xlive.ini", IniFile))
+        Console.WriteLine(@"Failed to save xlive.ini");
+    }
+
+    public static void SetVariable(string varName, string varValue, ref Dictionary<string, string> configDict)
+    {
+      if (configDict.ContainsKey(varName))
+        configDict[varName] = varValue;
+      else
+        configDict.Add(varName, varValue);
+    }
+
+    public static bool SaveConfigFile(string cfgFileName, Dictionary<string, string> configDict)
+    {
+      try
+      {
+        //Grab lines from the dictionary
+        var lines = configDict.Select(kvp => kvp.Key + " \"" + kvp.Value + "\"").ToList();
+
+        //Write all lines to xlive.ini and get out!
+        File.WriteAllLines(cfgFileName, lines.ToArray());
+        return true;
+      }
+      catch
+      {
+        return false;
+      }
+    }
+
+    private static bool LoadConfigFile(string cfgFileName, ref Dictionary<string, string> returnDict)
+    {
+      if (returnDict == null) throw new ArgumentNullException(nameof(returnDict));
+
+      if (!File.Exists(cfgFileName))
+        return false;
+
+      var lines = File.ReadAllLines(cfgFileName);
+      foreach (var line in lines)
+      {
+        var splitIdx = line.IndexOf(" ", StringComparison.Ordinal);
+        if (splitIdx < 0 || splitIdx + 1 >= line.Length)
+          continue; // Line isn't valid?
+        var varName = line.Substring(0, splitIdx);
+        var varValue = line.Substring(splitIdx + 1);
+
+        // Remove quotes
+        if (varValue.StartsWith("\""))
+          varValue = varValue.Substring(1);
+        if (varValue.EndsWith("\""))
+          varValue = varValue.Substring(0, varValue.Length - 1);
+
+        SetVariable(varName, varValue, ref returnDict);
+      }
+      return true;
+    }
+  }
+}
---
+++ H2V/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+
+namespace H2V
+{
+  public partial class MainWindow
+  {
+    //All your variables and constants so you can easily swap this out when the servers undoubtably change
+    const string Url = "http://h2v.online/update/version.txt";
+    private string _latestVersion;
+    private string _localVersion;
+
+    //Fires when the app opens (look at window_loaded for finer tuning)
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Icon_Click(object sender, RoutedEventArgs e)
+    {
+      Process.Start("http://www.h2v.online/");
+    }
+
+    private void BtnGenerate_Click(object sender, RoutedEventArgs e)
+    {
+      long uid = new long();
+
+      //while (!CheckUid(uid))
+      //loop here until uid is found that isn't taken
+
+      //Generate two ints and then append them to create our 16 digit long uid
+      Random rnd = new Random();
+      int uid1 = rnd.Next(10000000, 100000000);
+      int uid2 = rnd.Next(10000000, 100000000);
+      uid = Convert.ToInt64($"{uid1}{uid2}");
+      UidBox.Text = uid.ToString();
+
+      //Cfg.write(uid);
+      //Update uid value in cfg
+    }
+
+    /*
+    private bool CheckUid(long uid)
+    {
+      //This will check to see if the uid is taken (will need some database stuff setup)
+      if (uid != null)
+        return true;
+      return false;
+    }
+    */
+
+    private void BtnAction_Click(object sender, RoutedEventArgs e)
+    {
+      //Checks version and responds accordingly
+      Console.WriteLine(CheckVersion() ? "You are up to date!" : "You are not up to date! ");
+    }
+
+    private bool CheckVersion()
+    {
+      //Grab version number from the URL constant
+      _latestVersion = new WebClient().DownloadString(Url);
+
+      //Removes the extra linebreak (will be useful later for multiple variables in an update)
+      _latestVersion.Substring(0, _latestVersion.Length - 1);
+
+      //Check for xlive.dll file version number
+      _localVersion = FileVersionInfo.GetVersionInfo(Environment.SystemDirectory + "\\xlive.dll").FileVersion;
+
+      if (_localVersion == _latestVersion)
+        return true;
+      return false;
+    }
+  }
+}
