a7615d6adf7eab889d0876ed61bc3477a42cdb2a
iSupersniper <supersniperss@live.com>
2016-04-27 17:37:56 -0400

    changed build number

--- H2V/Properties/AssemblyInfo.cs
+++ H2V/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.0.0.6")]
-[assembly: AssemblyFileVersion("0.0.0.6")]
+[assembly: AssemblyVersion("0.0.0.7")]
+[assembly: AssemblyFileVersion("0.0.0.7")]
